f959abf2faa0bc055c7cfb0f63601d55eec07944
pihu1231 <93186329+pihu1231@users.noreply.github.com>
2022-09-21 17:02:17 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package core.java;
+
+/**
+ * @author User
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+
+	}
+
+}
