fd668ac4acd50650984dc24c9fbd1a92297ec2df
pihu1231 <93186329+pihu1231@users.noreply.github.com>
2022-09-23 13:41:31 +0530

    Add files via upload

---
+++ Teacher.java
@@ -0,0 +1,10 @@
+public class Teacher {
+    String sub;
+    String name;
+    void teach(){
+        System.out.println("I teach :" +sub);
+    }
+    void print(){
+        System.out.println("My name is :" +name);
+    }
+}
