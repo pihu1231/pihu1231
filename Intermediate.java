f0be73246820d4261a0703f479e4c1fc39d81193
pihu1231 <93186329+pihu1231@users.noreply.github.com>
2022-09-23 13:42:53 +0530

    Add files via upload

---
+++ Intermediate.java
@@ -0,0 +1,9 @@
+// This class is derived from Multilevel class and it will inherit the property of Multilevel class also.
+public class Intermediate extends Multilevel{
+    // Method and fields of Intermediate class.
+    int id;
+    void show(){
+        System.out.println("  This is a Intemediatory class");
+        System.out.println("My name is: " +name);
+    }
+}
