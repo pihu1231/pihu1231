9b24530f1c72b935e29b92e69abfa5e8f8128564
pihu1231 <93186329+pihu1231@users.noreply.github.com>
2022-09-27 17:50:23 +0530

    Add files via upload

---
+++ Hands-on Assignment/Relational.java
@@ -0,0 +1,34 @@
+package core.java;
+
+import java.util.Scanner;
+
+//Relational Operator program:
+public class Relational {
+//Main Method
+	public static void main(String[] args) {
+//		Create Scanner class to take input from user
+		Scanner sc = new Scanner(System.in);
+//		Getting input value from user :
+		System.out.println("Enter the first value: ");
+		int num1 = sc.nextInt();
+		System.out.println("Enter the Second value: ");
+		int num2 = sc.nextInt();
+		System.out.println("Enter the third value: ");
+		int num3 = sc.nextInt();
+//		Perform the equal operator:
+		System.out.println("Print the result of equal operator: " + (num1 == num2) + " "+ (num3==num2)+" "+ (num1 ==num3));
+//		perform Not Equal operator:
+		System.out.println("Print the result of not equal to operator: " + (num1 != num2)+ " "+ (num3!=num2)+" "+ (num1 !=num3));
+//		perform the Greater than operator:
+		System.out.println("Print the result of less than: " + (num1 < num2)+ " "+ (num3<num2)+" "+ (num1 < num3));
+//		perform the Greater than operator:
+		System.out.println("Print the result of greater than: " + (num1 > num2)+ " "+ (num3>num2)+" "+ (num1 >num3));
+//		perform the Greater than or equal to operator:
+		System.out.println("Print the result of greater than: " + (num3 <= num2)+ " "+ (num3<=num2)+" "+ (num1 <=num3));
+//		perform the Less than or equal to operator:
+		System.out.println("Print the result of greater than: " + (num1 >= num3)+ " "+ (num3>=num2)+" "+ (num1 >=num3));
+
+	}
+
+}
+
