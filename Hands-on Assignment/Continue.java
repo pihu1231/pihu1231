68c38e39ddb2cf1ed9b8b0f8060e9000c67e05af
pihu1231 <93186329+pihu1231@users.noreply.github.com>
2022-10-08 07:51:20 +0530

    Update Continue.java

--- Hands-on Assignment/Continue.java
+++ Hands-on Assignment/Continue.java
@@ -2,25 +2,23 @@ package core.java;
 
 import java.util.Scanner;
 
-// WAP to demonstrate continue statement using while loop:
+// WAP to demonstrate while loop continues until the entered number is positive:
 public class Continue {
 
 	public static void main(String[] args) {
+		int sum = 0;
 //		Create an object of Scanner class:
 		Scanner sc = new Scanner(System.in);
 //		Take the input from user:
 		System.out.println(" Enter your number:");
 		int x = sc.nextInt();
-//		while loop:
-		int i = x;
-		while (i <= 50) {
-			if (i == 5) {
-//				using continue statement:
-				i++;
-				continue; // Skip the rest statement
-			}
-			System.out.println(i);
-			i++;
+//		while loop continue until entered no is +ve :
+		while (x >= 0) {
+//			add only positive numbers: 
+			sum += x;
+			System.out.println("Enter your number");
+			x = sc.nextInt();
+			System.out.println("Sum = " + sum);
 		}
 		sc.close();
 	}
