c773cf74df6c36695d2d085c84f96942f7701981
pihu1231 <93186329+pihu1231@users.noreply.github.com>
2022-10-19 22:23:33 +0530

    Add files via upload

---
+++ Hands-on Assignment/ArrayString.java
@@ -0,0 +1,14 @@
+package core.java;
+
+public class ArrayString {
+
+	public static void main(String[] args) {
+//		Initialization  and Declaration of String Array:
+		String name[] = { "Aman", "parul", "Mitra", "Akansha" };
+//		traverse the Array to print the input of String Array:
+		for (int i = 0; i < name.length; i++) {
+			System.out.println(name[i]);
+		}
+	}
+
+}
