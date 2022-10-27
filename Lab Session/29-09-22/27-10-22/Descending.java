8e359a98dcc42d4ec1365ba528ccffb3b1c15781
pihu1231 <93186329+pihu1231@users.noreply.github.com>
2022-10-27 16:01:06 +0530

    Add files via upload

---
+++ Lab Session/29-09-22/27-10-22/Descending.java
@@ -0,0 +1,23 @@
+package array.java;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Descending {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Your String :");
+		String str = sc.next();
+//		Declaration of array:
+		char arr[] = str.toCharArray();
+//		Sort the array by built in method:
+		Arrays.sort(arr);
+		System.out.println("Print the descending order of string:");
+//		Print the array using for loop
+		for (int i = arr.length - 1; i >= 0; i--) {
+			System.out.println(arr[i] + "");
+		}
+		sc.close();
+	}
+
+}
